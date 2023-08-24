ac8cb81e54bf38b616feeb804caa1ee846c4ef91
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2023-08-24 20:48:38 +0400

    Create procedure.sql

---
+++ procedure.sql
@@ -0,0 +1,13 @@
+create or replace procedure update_employee(p_employee_id number,p_increased_commission_pct out number) is
+v_commission_pct number;
+begin
+    select commission_pct into v_commission_pct from employees where employee_id=p_employee_id;
+    p_increased_commission_pct:=v_commission_pct+0.2;
+    update employees set commission_pct=p_increased_commission_pct  where employee_id=p_employee_id;
+exception
+   
+     when no_data_found then
+    dbms_output.put_line('Error: Employee not found');
+    when others then 
+    dbms_output.put_line('An error occurred: ' || SQLERRM); 
+end;
